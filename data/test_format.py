0381c4c92b71ae3a0938915db77e4d83d7b65f86
Schmiddy2000 <luca.schmitt@gmx.net>
2024-09-15 22:36:16 +0200

    Updating...

--- data/test_format.py
+++ data/test_format.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 from scipy.optimize import curve_fit
+from scipy import stats
 
 
 base_path = "/Users/lucas1/Desktop/Uni/Physiklabor B/Physiklabor für Fortgeschrittene/Versuch 4 - Supraleiter/"
@@ -13,9 +14,16 @@ def get_path(index: int):
     return base_path + f"Messung_{index}.csv"
 
 
-df = pd.read_csv(get_path(25))
+df = pd.read_csv(get_path(10))
 
 print(df.columns)
+
+
+def wire_voltage_to_temperature(voltages: np.array) -> np.array:
+    voltage_at_R_min = 191.7e-3
+    voltage_at_0 = 0.45e-3
+
+    return -(voltages - voltage_at_0) * 196 / (voltage_at_R_min - voltage_at_0)
 
 
 columns = ['Zeit t / s', 'Spannung U_A1 / V']
@@ -24,11 +32,46 @@ df = df[columns]
 x = df[columns[0]].to_numpy()
 y = df[columns[1]].to_numpy()
 
-# 19.26 -> 19.4
-remove_indices = [int((50 * val) / 100) for val in range(1926, 1940) if val % 2 == 0]
-remove_indices += [i for i in range(0, 12 * 50)]
+y = wire_voltage_to_temperature(y)
 
-print(remove_indices)
+x_lin = np.linspace(-0.1 * max(x), 1.1 * max(x))
+
+# Perform linear regression using scipy
+slope, intercept, r_value, p_value, std_err = stats.linregress(x, y)
+
+print(f"Slope: {slope}")
+print(f"Intercept: {intercept}")
+print(f"R-squared: {r_value**2}")
+print(f"P-value: {p_value}")
+print(f"Standard error: {std_err}")
+
+
+# Predict y values using the regression model
+def lin_func(x, a, b):
+    return a * x + b
+
+
+# Plot the original data and the regression line
+plt.figure(figsize=(12, 5))
+plt.scatter(x, y, color='green', s=5, label='Data Points')
+plt.plot(x_lin, lin_func(x_lin, slope, intercept), color='black', label='Regression Line')
+plt.plot(x_lin, lin_func(x_lin, slope, intercept + 1.25), color='red', ls='--', label='Offset of +1.25mV')
+plt.plot(x_lin, lin_func(x_lin, slope, intercept - 2.25), color='orange', ls='--',  label='Offset of -2.25mV')
+plt.xlabel('Time in [s]', fontsize=13)
+plt.ylabel('Voltage in [mV]', fontsize=13)
+plt.title('Linear regression over voltage fluctuations around room temperature', fontsize=16)
+plt.legend()
+plt.xlim(-25, 325)
+plt.tight_layout()
+plt.savefig('current_fluctuation_visualization.png', dpi=200)
+plt.show()
+
+
+# 19.26 -> 19.4
+# remove_indices = [int((50 * val) / 100) for val in range(1926, 1940) if val % 2 == 0]
+# remove_indices += [i for i in range(0, 12 * 50)]
+#
+# print(remove_indices)
 
 
 # Define the piecewise function with a Heaviside step function
@@ -41,34 +84,34 @@ print(remove_indices)
 
 # The following part still has to be fixed / automated
 
-def model_func(t, A, D, E):
-    linear_part = A
-    exp_part = A * np.exp(-D * (t - 12)) + E
-    heaviside = np.heaviside(t - 12, 1)  # 1 for t >= t0, 0 for t < t0
-    return linear_part * (1 - heaviside) + exp_part * heaviside
-
-
-# Example data (replace this with your actual data)
-t_data = np.delete(x, remove_indices)
-y_data = np.delete(y, remove_indices)
-
-# Initial guess for parameters [A, B, C, D, E, t0]
-initial_guess = [1, 0.01, 0]
-
-# Perform curve fitting
-popt, pcov = curve_fit(model_func, t_data, y_data, p0=initial_guess)
-
-# Extract fitted parameters
-A_fit, D_fit, E_fit = popt
+# def model_func(t, A, D, E):
+#     linear_part = A
+#     exp_part = A * np.exp(-D * (t - 12)) + E
+#     heaviside = np.heaviside(t - 12, 1)  # 1 for t >= t0, 0 for t < t0
+#     return linear_part * (1 - heaviside) + exp_part * heaviside
+#
+#
+# # Example data (replace this with your actual data)
+# t_data = np.delete(x, remove_indices)
+# y_data = np.delete(y, remove_indices)
+#
+# # Initial guess for parameters [A, B, C, D, E, t0]
+# initial_guess = [1, 0.01, 0]
+#
+# # Perform curve fitting
+# popt, pcov = curve_fit(model_func, t_data, y_data, p0=initial_guess)
+#
+# # Extract fitted parameters
+# A_fit, D_fit, E_fit = popt
 
 # Plot the data and the fit
-plt.plot(t_data, y_data, label="Data")
+plt.scatter(x, y, label="Data", s=5)
 # plt.plot(t_data, model_func(t_data, *popt), label="Fitted Curve")
 # plt.axvline(x=t0_fit, color='r', linestyle='--', label=f'Transition Time t0 = {t0_fit:.2f} s')
 plt.legend()
 plt.xlabel("Time (ms)")
 plt.ylabel("Current (A)")
-plt.show()
+# plt.show()
 
 # plt.figure(figsize=(12, 5))
 #
---
+++ tests/resistance_and_temperature.py
@@ -0,0 +1,26 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+A = 3.9083e-3
+B = -5.775e-7
+C = -4.183e-12
+
+# U = RI
+
+
+def wire_voltage_to_temperature(voltages: np.array) -> np.array:
+    voltage_at_R_min = 191.7e-3
+    voltage_at_0 = 0.45e-3
+
+    return -(voltages - voltage_at_0) * 196 / (voltage_at_R_min - voltage_at_0)
+
+
+def R_0_coefficient(temperature: int):
+    return 1 + A * temperature + B * temperature ** 2 + C * (temperature - 100) * temperature ** 3
+
+
+lin_temp = np.linspace(-200, 0, 250)
+
+plt.plot(lin_temp, 100 * R_0_coefficient(lin_temp))
+plt.show()
